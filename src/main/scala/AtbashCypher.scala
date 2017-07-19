a3dd590ae75566e21ca24d312cbadef02ebb2a73
AlexRimmerQA <alex.rimmer@qa.com>
2017-07-19 12:28:34 +0100

    Changed format of code to better fit with codacy code stlye, removal of any else if. Changed to just if and else

--- src/main/scala/AtbashCypher.scala
+++ src/main/scala/AtbashCypher.scala
@@ -5,12 +5,15 @@
 class AtbashCypher {
  def toCypher(input: String): String = {
    input.map(x =>
-     if(x >= 'a' && x <= 'z') {
-       ('z' - (x - 'a')).toChar
-     } else if(x >= 'A' && x <= 'Z') {
-       ('Z' - (x - 'A')).toChar
+     if(x.isLetter) {
+       if(x.isLower) {
+         ('z' - (x - 'a')).toChar
+       } else {
+         ('Z' - (x - 'A')).toChar
+       }
      } else {
        x
-     }).toString
+     }
+   ).toString
  }
 }
